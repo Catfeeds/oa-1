ba4b32e41774982ef67739aed0645dc9792e270d
wangyingjie <930055912@qq.com>
2018-10-11 11:54:53 +0800

    提交修改打卡表的leave_id类型的迁移文件

---
+++ database/migrations/2018_10_11_113608_change_daily_detail_table_leave_id_to_string.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDailyDetailTableLeaveIdToString extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('attendance_daily_detail', function (Blueprint $table) {
+            $table->string('leave_id', 50)->nullable()->change()->comment = '假期申请单ID，默认为空';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('attendance_daily_detail', function (Blueprint $table) {
+            $table->unsignedInteger('leave_id')->nullable()->comment = '假期申请单ID，默认为空';
+        });
+    }
+}
