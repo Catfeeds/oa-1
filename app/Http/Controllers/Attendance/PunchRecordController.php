0b5565fc0e7047fabc17b7f95d3630f44150296d
jiangweiming <329403630@qq.com>
2018-10-13 16:07:06 +0800

    更新 调试

--- app/Http/Controllers/Attendance/PunchRecordController.php
+++ app/Http/Controllers/Attendance/PunchRecordController.php
@@ -141,10 +141,9 @@ class PunchRecordController extends Controller
                     //线上环境格式
                     list($month, $day, $year) = explode('-', $ts);
                     $calendar = Calendar::with('punchRules')
-                        ->where(['year' => $year, 'month' => $month, 'day' => $day])
+                        ->where(['year1' => $year, 'month' => $month, 'day' => $day])
                         ->first();
-                    print_r($year . '-' . $month . '-' . $day);
-                    print_r(strtotime($year . '-' . $month . '-' . $day));
+
                     dump($calendar);
                     if (empty($calendar->punch_rules_id)) {
                         $isOK = false;
