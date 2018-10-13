1b6766841459835898fd7a1b2d825053cf13ec7a
jiangweiming <329403630@qq.com>
2018-10-13 16:00:02 +0800

    更新 调试

--- app/Http/Controllers/Attendance/PunchRecordController.php
+++ app/Http/Controllers/Attendance/PunchRecordController.php
@@ -112,7 +112,7 @@ class PunchRecordController extends Controller
         $isOK = true;
        //事务开启
         DB::beginTransaction();
-        try {
+        //try {
             //reader读取excel内容
             \Excel::load(storage_path($punchRecord->annex), function ($reader) use ($punchRecord, $isOK) {
 
@@ -143,8 +143,8 @@ class PunchRecordController extends Controller
                     $calendar = Calendar::with('punchRules')
                         ->where(['year' => $year, 'month' => $month, 'day' => $day])
                         ->first();
-
-
+                    print_r($year . '-' . $month . '-' . $day);
+                    dump($calendar);
                     if (empty($calendar->punch_rules_id)) {
                         $isOK = false;
                         $msgArr[] = '未匹配到[' . $year . '-' . $month . '-' . $day . ']日期考勤规则设置,导入失败!';
@@ -236,14 +236,14 @@ class PunchRecordController extends Controller
                 $punchRecord->update(['log_file' => $logFile, 'status' => 3]);
 
             });
-      } catch (\Exception $e) {
+/*      } catch (\Exception $e) {
             //事务回滚
             DB::rollBack();
             $punchRecord->update(['status' => 2]);
 
             flash('生成失败，生成文件有误，无法解析!', 'danger');
             return redirect()->route('daily-detail.review.import.info');
-        }
+        }*/
         //事务提交
         DB::commit();
         flash(trans('att.生成成功员工每日打卡明细'), 'success');
